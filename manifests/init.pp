e38c1a4cf56ffa05e51dde587124a43b46616f76
Adam Breaux <breauxaj@gmail.com>
2014-10-08 07:54:30 -0400

    standard preamble

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,31 @@
+# Class: haproxy
+#
+# This class installs the haproxy package
+#
+# Parameters:
+#
+#  ensure: (default latest)
+#    Determine the state of the packages
+#
+# Actions:
+#   - Installs the haproxy package
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'haproxy':
+#      ensure => 'latest'
+#    }
+#
+#  To remove the installation, use:
+#
+#    class { 'haproxy':
+#      ensure => 'absent'
+#    }
+#
 class haproxy (
   $ensure = 'latest'
 ){
